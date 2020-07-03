277f4ca8a747106eea48387e3060cd64b2b5706c
George Miao <gm@georgemiao.com>
2020-07-03 23:41:06 +0800

    remove file from git

--- CREDENTIAL.py
+++
@@ -1,8 +0,0 @@
-key_id = "002cec7b72e56240000000001"
-applicationKey = "K002nk+sysETpHRRdc1oIDWfIMTjuoM"
-
-# Cloudflare
-account_id = "595a22d447b6f579a3bac7613e8aec5e"
-cf_token = "b1b94888aa7cd0a44a9b8666dbc4b7b21ac3a"
-cf_email = "gm@georgemiao.com"
-namespace = "9fa86623bf4640c393e39fdee3847b6c"
